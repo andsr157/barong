4b62b6012d4f12a0a1afa578a0e4121179366e98
andsr157 <andikasatrio159@gmail.com>
2024-03-14 00:38:59 +0700

    feat: add transaction card dan small button components

---
+++ components/button/Small.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  color: string
+}>()
+</script>
+
+<template>
+  <button
+    class="min-w-[86px] h-8 rounded-[20px] text-white text-[11px] font-semibold"
+    :class="color"
+  >
+    {{ label }}
+  </button>
+</template>
---
+++ components/card/TransactionUser.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+defineProps<{
+  detailSampah: string
+  status: string
+  review?: number | null
+}>()
+</script>
+
+<template>
+  <div
+    class="flex flex-col justify-between w-full min-h-[145px] px-[14px] py-3 rounded-[10px] shadow-[0px_0px_10px_rgba(0,0,0,0.25)]"
+  >
+    <div class="flex justify-between">
+      <h3 class="text-sm font-semibold text-brg-primary-dark w-40">
+        {{ detailSampah }}
+      </h3>
+      <span
+        class="text-[11px] text-brg-primary font-semibold text-end w-[105px]"
+        >{{ status }}</span
+      >
+    </div>
+    <div>
+      <div
+        v-if="status === 'diambil' || status === 'selesai'"
+        class="flex gap-x-2 items-center"
+      >
+        <NuxtImg
+          src="/assets/dummy-profile.png"
+          alt="foto pengepul"
+          size="32px"
+          width="32"
+          height="32"
+        />
+        <span class="text-[11px] font-medium text-brg-primary-dark mt-1">
+          Mulyanto
+        </span>
+      </div>
+      <div>
+        <p class="text-[13px]" v-if="status === 'mencari pengepul'">
+          Permintaanmu sedang diproses. Silahkan menunggu.
+        </p>
+        <p class="text-[13px]" v-else-if="status === 'dibatalkan'">
+          Transaksi anda telah dibatalkan. Anda bisa mengajukan kembali.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex justify-between items-end">
+      <span
+        class="text-xs text-[#72798E] font-medium"
+        v-if="status === 'selesai' || status === 'dibatalkan'"
+        >12 Januari 2024</span
+      >
+      <span v-else class="text-xs text-[#72798E] font-medium">12:30</span>
+      <ButtonSmall
+        v-if="status === 'diambil'"
+        label="lacak"
+        color="bg-brg-secondary"
+      />
+      <ButtonSmall
+        v-else-if="status === 'mencari pengepul'"
+        label="batalkan"
+        color="bg-brg-red"
+      />
+      <ButtonSmall
+        v-else-if="status === 'dibatalkan'"
+        label="Buat Transaksi"
+        color="bg-brg-secondary"
+        class="px-3"
+      />
+      <ButtonSmall
+        v-else-if="status === 'selesai' && review !== null"
+        label="Beri Nilai"
+        color="bg-brg-secondary"
+        class="px-3"
+      />
+    </div>
+  </div>
+</template>
