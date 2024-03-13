8bf1d2311ac9d299219a4756c3514616d61b71e2
andsr157 <andikasatrio159@gmail.com>
2024-03-13 21:33:02 +0700

    feat: add user home hero section

---
+++ components/card/SummaryTransaction.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { toCurrency } from "~/composables/helpers"
+defineProps<{
+  total: number
+}>()
+</script>
+
+<template>
+  <div
+    class="p-3 rounded-[10px] border-[1px] border-black border-opacity-10 text-brg-primary-dark"
+  >
+    <div class="flex mb-2 gap-x-1">
+      <Icon name="solar:graph-bold" class="" size="24px" />
+      <span class="font-medium">Total transaksi</span>
+    </div>
+    <h2 class="text-2xl font-semibold mb-">
+      {{ toCurrency(total) }}
+    </h2>
+    <span class="text-xs font-medium text-brg-light-gray"
+      >Perolehan penjualan sampah anda selama ini</span
+    >
+  </div>
+</template>
