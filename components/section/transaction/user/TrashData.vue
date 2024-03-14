48aa2fe82b7e16ba585e0423752f162ad8a23c03
andsr157 <andikasatrio159@gmail.com>
2024-03-14 23:05:48 +0700

    feat(transaction): add button component and dropdown component

---
+++ components/section/transaction/user/TrashData.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { TRANSACTION } from "~/constants/trash.constants"
+</script>
+
+<template>
+  <section class="mt-6 pb-48">
+    <h2 class="text-brg-primary-dark font-semibold">Data Sampah</h2>
+    <div class="my-2">
+      <div
+        class="flex gap-x-1 mb-2 h-[38px]"
+        v-for="trash in TRANSACTION[0].detailSampah"
+      >
+        <div
+          class="w-48 h-[38px] border-[1px] border-brg-light-gray rounded-[10px] px-3 py-[10px] shrink-0"
+        >
+          <p class="text-xs text-brg-primary-dark font-medium">
+            {{ trash.category }} ! {{ trash.subcategory }}
+          </p>
+        </div>
+        <Input
+          wrapper-class="!px-1"
+          input-class="text-brg-primary !text-sm !font-semibold text-end pe-1"
+        >
+          <template #suffix>
+            <span class="text-brg-primary text-[11px] font-medium pt-1"
+              >Kg</span
+            >
+          </template>
+        </Input>
+        <button
+          class="bg-transparent px-3 py-[1px] border-[1px] border-brg-red rounded-[10px] max-h-[38px]"
+        >
+          <Icon name="mdi:delete" class="text-brg-red" size="21px" />
+        </button>
+      </div>
+
+      <div
+        class="flex gap-x-2 justify-end text-xs text-brg-primary-dark font-medium mt-3"
+      >
+        <p>Total Sampah</p>
+        <p class="text-brg-primary text-sm">10.0</p>
+        <p>Kg</p>
+      </div>
+    </div>
+
+    <ButtonLarge label="Tambah Sampah" class="mx-auto" />
+
+    <SectionTransactionUserTrashForm class="mt-3" />
+  </section>
+</template>
