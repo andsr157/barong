221f453fffe6860c74dd250dbd25351309f8711c
andsr157 <andikasatrio159@gmail.com>
2024-03-14 19:55:09 +0700

    feat: add input component

---
+++ components/section/transaction/user/TrashImage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const data = ref(null)
+</script>
+<template>
+  <section>
+    <h2 class="text-brg-primary-dark font-semibold mb-2">Foto Sampah</h2>
+    <CardImageGallery with-delete />
+  </section>
+</template>
---
+++ pages/user/transaction/Add.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank",
+})
+</script>
+
+<template>
+  <Header title="Transaksi" />
+  <div class="px-6">
+    <section>
+      <div class="text-brg-primary-dark mt-[30px] mb-6">
+        <h2 class="font-semibold mb-2">Keranjang Sampah</h2>
+        <p class="font-medium text-[11px] text-opacity-70">
+          Input sampah, pilih kategori dan masukkan perkiraan berat sampahmu
+        </p>
+      </div>
+    </section>
+
+    <SectionTransactionUserTrashImage />
+  </div>
+</template>
