d319bf2aad3f554d5a6f4d0b5a5cbd3d0bd3a640
andsr157 <andikasatrio159@gmail.com>
2024-03-14 19:52:20 +0700

    feat(header): implement navigation back header

--- components/Header.vue
+++ components/Header.vue
@@ -2,12 +2,25 @@
 defineProps<{
   title: string
 }>()
+const router = useRouter()
+const goBack = () => {
+  if (window.history.state.back !== null) {
+    router.back()
+  } else {
+    router.push("/user")
+  }
+}
 </script>
 
 <template>
   <header>
     <div class="grid grid-cols-3 px-6 py-4 items-center">
-      <Icon name="mdi:arrow-back" size="24px" class="text-brg-primary-dark" />
+      <Icon
+        name="mdi:arrow-back"
+        size="24px"
+        class="text-brg-primary-dark"
+        @click="goBack()"
+      />
       <h1 class="text-brg-primary-dark font-semibold text-[20px]">
         {{ title }}
       </h1>
