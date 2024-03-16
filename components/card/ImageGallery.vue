ca1d7d6f2c10c8e14eaa4a8e92b6386c06739207
andsr157 <andikasatrio159@gmail.com>
2024-03-16 12:34:52 +0700

    feat: input file component and preview image input

--- components/card/ImageGallery.vue
+++ components/card/ImageGallery.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 defineProps<{
+  url?: string
   withDelete?: boolean
 }>()
 </script>
 
 <template>
   <div
+    v-if="url"
     :class="`${
       withDelete
         ? 'border-[1px] border-brg-light-gray rounded-[10px] overflow-hidden'
@@ -13,16 +15,32 @@ defineProps<{
     }`"
   >
     <NuxtImg
-      src="/assets/dummy-trash.png"
-      class="object-cover"
+      v-if="url"
+      :src="url"
+      class="object-cover h-[159px]"
       width="312"
       height="159"
     />
     <div v-if="withDelete" class="w-full px-2 py-[6px]">
-      <div class="flex gap-x-1 items-center">
+      <div class="flex gap-x-1 items-center" @click="$emit('deleteImage')">
         <Icon name="mdi:delete" size="20px" class="text-brg-gray" />
         <span class="text-[10px] text-brg-gray font-medium">Hapus Foto</span>
       </div>
+    </div>
+  </div>
+  <div
+    v-else
+    class="w-full h-[180px] border-[1px] border-brg-light-gray rounded-[10px] flex justify-center items-center"
+  >
+    <div class="flex flex-col max-w-max justify-center">
+      <Icon
+        name="fluent:camera-24-filled"
+        size="36px"
+        class="text-brg-light-gray !mx-auto"
+      />
+      <p class="text-[11px] text-brg-gray text-center mt-2">
+        Upload foto sampah(wajib)
+      </p>
     </div>
   </div>
 </template>
