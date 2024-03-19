318ebc9630bf560112388b3dd96f6657c5e66ca6
andsr157 <andikasatrio159@gmail.com>
2024-03-19 17:43:57 +0700

    feat: implement pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
@@ -8,9 +9,61 @@ export default defineNuxtConfig({
     'nuxt-icon',
     '@vee-validate/nuxt',
     '@nuxt/image',
-    'nuxt-swiper'
+    'nuxt-swiper',
+    '@vite-pwa/nuxt'
   ],
 
+  pwa: {
+    manifest: {
+      name: 'Barong',
+      short_name: 'Barong',
+      description: 'Baring adalah aplikasi yang memudahkan Anda untuk melakukan jual beli sampah atau rongsok secara mudah dan efisien. Temukan pengepul terdekat, jual sampah Anda, dan dapatkan keuntungan tambahan dengan Baring.',
+      icons: [
+        {
+          "src": "/icon_192x192.png",
+          "type": "image/png",
+          "sizes": "192x192"
+        },
+        {
+          "src": "/icon_256x256.png",
+          "type": "image/png",
+          "sizes": "256x256"
+        },
+        {
+          "src": "/icon_384x384.png",
+          "type": "image/png",
+          "sizes": "384x384"
+        },
+        {
+          "src": "/icon_512x512.png",
+          "type": "image/png",
+          "sizes": "512x512"
+        },
+      ],
+      screenshots: [
+        {
+          "src": "/screenshot1.png",
+          "sizes": "640x1136",
+          "type": "image/png"
+        },
+        {
+          "src": "/screenshot2.png",
+          "sizes": "1136x640",
+          "type": "image/png"
+        }
+
+      ],
+      "theme_color": "#307FF5",
+      "background_color": "#ffffff"
+    },
+    workbox: {
+      navigateFallback: '/user'
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
+    }
+  },
 
   runtimeConfig: {
     public: {
@@ -18,6 +71,10 @@ export default defineNuxtConfig({
       apiurl: process.env.API_URL
     }
   },
+
+
+
+
 
   veeValidate: {
     autoImports: true
