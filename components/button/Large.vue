30b5b4c7985ad6cc3373627610f1e0080d604c8c
andsr157 <andikasatrio159@gmail.com>
2024-03-19 14:00:35 +0700

    feat: add component partnerProfile card and fix trash estimate,total dan stepper component

--- components/button/Large.vue
+++ components/button/Large.vue
@@ -2,16 +2,19 @@
 interface Props {
   label: string
   color?: string
+  disabled?: boolean
 }
 withDefaults(defineProps<Props>(), {
   color: "bg-brg-primary",
+  disabled: false,
 })
 </script>
 
 <template>
   <button
     class="w-[292px] h-10 rounded-[20px] text-white text-[11px] font-semibold"
-    :class="color"
+    :class="`${disabled ? 'bg-brg-light-gray cursor-not-allowed' : color}`"
+    :disabled="disabled"
   >
     {{ label }}
   </button>
--- components/card/TrashEstimate.vue
+++ components/card/TrashEstimate.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-defineProps<{
+import { toCurrency } from "~/composables/helpers"
+interface Props {
   category: string
   subcategory: string
   weight: number
-  minPrice: number
-  maxPrice: number
-}>()
+  finalPrice?: number
+  minPrice?: number
+  maxPrice?: number
+  type?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  type: "default",
+  finalPrice: 0,
+})
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="w-full" v-if="type === 'default'">
     <div class="text-brg-primary-dark flex justify-between mb-1">
       <h3 class="text-[13px]">
         <span>{{ category }}</span
@@ -28,6 +36,23 @@ defineProps<{
         ><span class="font-semibold">{{ minPrice }}</span
         ><span class="font-light"> s.d </span><span class="font-light">Rp</span
         ><span class="font-semibold">{{ maxPrice }}</span>
+      </p>
+    </div>
+  </div>
+
+  <div class="w-full" v-else-if="type === 'finish'">
+    <div class="text-brg-primary-dark grid grid-cols-5 justify-between mb-1">
+      <h3 class="text-[13px] col-span-3">
+        <span>{{ category }}</span
+        ><span class="font-medium"> | {{ subcategory }}</span>
+      </h3>
+
+      <p class="text-[13px] text-brg-primary-dark font-medium text-end">
+        {{ toCurrency(finalPrice) }}
+      </p>
+      <p class="text-sm text-end">
+        <span class="font-semibold">{{ weight }}</span
+        ><span> Kg</span>
       </p>
     </div>
   </div>
