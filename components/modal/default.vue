963a6c02afb8338f717476e3425ca0b66acfc854
andsr157 <andikasatrio159@gmail.com>
2024-03-20 00:10:21 +0700

    feat: add modal component

---
+++ components/modal/default.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+interface Props {
+  title?: string
+  desc?: string
+  labelConfirmation?: string
+  labelColor?: string
+  isShow: boolean
+  emitFunction: string
+}
+
+withDefaults(defineProps<Props>(), {
+  title: "modal",
+  desc: "Apakah anda yakin ?",
+  labelColor: "text-brg-primary-dark",
+})
+
+const emit = defineEmits()
+</script>
+
+<template>
+  <ClientOnly>
+    <transition name="fade-overlay">
+      <div
+        v-if="isShow"
+        class="w-full h-screen fixed z-40 bg-black bg-opacity-20 flex justify-center items-center"
+      ></div>
+    </transition>
+
+    <transition name="fade">
+      <div
+        v-if="isShow"
+        class="fixed w-full z-50 top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 px-6"
+      >
+        <div
+          class="w-full flex flex-col items-center py-6 rounded-[10px] bg-white"
+        >
+          <h1 class="text-brg-primary-dark font-bold mb-1">{{ title }}</h1>
+          <p class="text-brg-primary-dark text-[11px] w-[80%] text-center">
+            {{ desc }}
+          </p>
+          <div
+            class="grid grid-cols-2 w-full text-sm font-semibold mt-8 text-center"
+          >
+            <span class="text-brg-light-gray" @click="emit('closeModal')"
+              >Tidak</span
+            >
+            <span :class="labelColor" @click="emit(emitFunction)">{{
+              labelConfirmation
+            }}</span>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </ClientOnly>
+</template>
+
+<style>
+.fade-overlay-enter-active,
+.fade-overlay-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-overlay-enter-from,
+.fade-overlay-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
