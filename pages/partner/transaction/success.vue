a9e64ff15546b4ddad85b818d9da3d34226e43bd
andsr157 <andikasatrio159@gmail.com>
2024-03-20 16:08:14 +0700

    feat: transaction success pages

--- pages/partner/transaction/success.vue
+++ pages/partner/transaction/success.vue
@@ -2,14 +2,53 @@
 definePageMeta({
   layout: "blank",
 })
+const router = useRouter()
+const redirectToHome = () => {
+  setTimeout(() => {
+    router.push("/partner")
+  }, 5000)
+}
+
+const visible = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    visible.value = true
+  }, 500)
+  redirectToHome()
+})
 </script>
 
 <template>
   <div
     class="w-full h-screen bg-brg-primary flex justify-center items-center px-6"
   >
-    <CardTransactionSuccess
-      message="Terima kasih atas kerja keras anda  dan tetap semangat!!"
-    />
+    <transition name="bounceFromBottom">
+      <CardTransactionSuccess
+        v-if="visible"
+        message="Terima kasih atas kerja keras anda  dan tetap semangat!!"
+      />
+    </transition>
   </div>
 </template>
+
+<style>
+@keyframes bounceFromBottom {
+  0% {
+    transform: translateY(100%);
+  }
+  60% {
+    transform: translateY(-20%);
+  }
+  80% {
+    transform: translateY(10%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.bounceFromBottom-enter-active {
+  animation: bounceFromBottom 0.5s ease;
+}
+</style>
--- pages/user/transaction/Success.vue
+++ pages/user/transaction/Success.vue
@@ -2,14 +2,52 @@
 definePageMeta({
   layout: "blank",
 })
+const router = useRouter()
+const redirectToHome = () => {
+  setTimeout(() => {
+    router.push("/user")
+  }, 5000)
+}
+
+const visible = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    visible.value = true
+  }, 500)
+  redirectToHome()
+})
 </script>
 
 <template>
   <div
     class="w-full h-screen bg-brg-primary flex justify-center items-center px-6"
   >
-    <CardTransactionSuccess
-      message="Terima kasih, sekali lagi anda berkontribusi dalam rantai daur ulang sampah"
-    />
+    <transition name="bounceFromBottom">
+      <CardTransactionSuccess
+        v-if="visible"
+        message="Terima kasih, sekali lagi anda berkontribusi dalam rantai daur ulang sampah"
+      />
+    </transition>
   </div>
 </template>
+<style>
+@keyframes bounceFromBottom {
+  0% {
+    transform: translateY(100%);
+  }
+  60% {
+    transform: translateY(-20%);
+  }
+  80% {
+    transform: translateY(10%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.bounceFromBottom-enter-active {
+  animation: bounceFromBottom 0.5s ease;
+}
+</style>
