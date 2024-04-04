3ebc0f761429cbfa88ce15eb222af707f44f71f1
andsr157 <andikasatrio159@gmail.com>
2024-04-04 16:24:32 +0700

    feat: redirect homepage

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank",
+  middleware: "auth",
+})
+
+onBeforeMount(() => {
+  const { data: user } = <any>useAuth()
+  const role = user.value?.user?.role
+  if (role === "user") {
+    useRouter().push("/user")
+  } else if (role === "partner") {
+    useRouter().push("/partner")
+  }
+})
+</script>
+<template></template>
