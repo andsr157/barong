ac3ce454e029fe7d2180eee246f97216f495b5c1
andsr157 <andikasatrio159@gmail.com>
2024-04-23 13:16:33 +0700

    feat: testing supabase realtime on prod

--- components/section/home/user/Hero.vue
+++ components/section/home/user/Hero.vue
@@ -3,18 +3,25 @@ import axios from "axios"
 
 const total = ref()
 const { data: user } = <any>useAuth()
+const nuxt = useNuxtApp()
+
+const { data, status } = useFetch("/api/v1/dashboard/user", {
+  getCachedData(key) {
+    return nuxt.payload.data[key] || nuxt.static.data[key]
+  },
+})
 
 onMounted(async () => {
-  try {
-    const res = await axios.get("/api/v1/dashboard/user")
-    if (res) {
-      total.value = res.data.data.totalAmount
-    } else {
-      console.error("failed fetch api")
-    }
-  } catch (error) {
-    console.error(error)
-  }
+  // try {
+  //   const res = await axios.get("/api/v1/dashboard/user")
+  //   if (res) {
+  //     total.value = res.data.data.totalAmount
+  //   } else {
+  //     console.error("failed fetch api")
+  //   }
+  // } catch (error) {
+  //   console.error(error)
+  // }
 })
 </script>
 
@@ -23,6 +30,8 @@ onMounted(async () => {
     <h1 class="text-2xl text-brg-primary-dark font-bold mb-4">
       Hai {{ user.user.name }}
     </h1>
-    <CardSummaryTransaction :total="total" />
+    <CardSummaryTransaction
+      :total="status === 'success' ? data.data.totalAmount : 0"
+    />
   </section>
 </template>
