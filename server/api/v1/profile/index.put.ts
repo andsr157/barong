3f9e6c81e44064c9129f206e9e5ac5e551abe639
siihasann <hasannajib3@gmail.com>
2024-04-24 17:06:12 +0700

    api edit profil

---
+++ server/api/v1/profile/index.put.ts
@@ -0,0 +1,25 @@
+import { prisma } from '~/composables/prisma'
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+
+    try {
+        const updatedProfile = await prisma.users.update({
+            where: {
+                id: body.id
+            },
+            data: {
+                name: body.name,
+                email: body.email,
+                telp: body.telp,
+
+            },
+        });
+        return updatedProfile
+
+    } catch (error) {
+        // Menangani kesalahan yang mungkin terjadi selama pembaruan profile
+        console.error('Error updating profile:', error);
+        throw new Error('Error updating profile');
+    }
+});
